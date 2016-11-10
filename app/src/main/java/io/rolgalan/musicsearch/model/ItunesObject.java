8d5d0b2997cc9c7d32412445f1abdcdcb4605413
rolgalan <rolgalan@gmail.com>
2016-11-10 21:26:02 +0100

    Add basic data model

---
+++ app/src/main/java/io/rolgalan/musicsearch/model/ItunesObject.java
@@ -0,0 +1,18 @@
+package io.rolgalan.musicsearch.model;
+
+/**
+ * Created by Roldán Galán on 10/11/2016.
+ */
+
+public class ItunesObject {
+    String wrapperType;
+    String explicitness;
+    String kind;
+    String trackName;
+    String artistName;
+    String collectionName;
+    String censoredName;
+    String artworkUrl60;
+    String artworkUrl100;
+    String viewURL;
+}
---
+++ app/src/main/java/io/rolgalan/musicsearch/model/ItunesTrack.java
@@ -0,0 +1,17 @@
+package io.rolgalan.musicsearch.model;
+
+/**
+ * Created by Roldán Galán on 10/11/2016.
+ */
+
+public class ItunesTrack extends ItunesObject {
+    String previewUrl;
+    String trackPrice;
+    String collectionPrice;
+    String trackCount;
+    String trackTimeMillis;
+    String country;
+    String currency;
+    String primaryGenreName;
+
+}
