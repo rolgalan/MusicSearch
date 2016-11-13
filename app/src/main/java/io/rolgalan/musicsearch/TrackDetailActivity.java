ade387e0bb735f1fca84c032ee967ccc3402c0f6
rolgalan <rolgalan@gmail.com>
2016-11-13 21:01:32 +0100

    Fix getting track position arguments

--- app/src/main/java/io/rolgalan/musicsearch/TrackDetailActivity.java
+++ app/src/main/java/io/rolgalan/musicsearch/TrackDetailActivity.java
@@ -4,11 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
 /**
  * An activity representing a single Track detail screen. This
@@ -53,8 +53,8 @@ public class TrackDetailActivity extends AppCompatActivity {
             // Create the detail fragment and add it to the activity
             // using a fragment transaction.
             Bundle arguments = new Bundle();
-            arguments.putString(TrackDetailFragment.ARG_ITEM_ID,
-                    getIntent().getStringExtra(TrackDetailFragment.ARG_ITEM_ID));
+            arguments.putInt(TrackDetailFragment.ARG_ITEM_ID,
+                    getIntent().getIntExtra(TrackDetailFragment.ARG_ITEM_ID, 0));
             TrackDetailFragment fragment = new TrackDetailFragment();
             fragment.setArguments(arguments);
             getSupportFragmentManager().beginTransaction()
